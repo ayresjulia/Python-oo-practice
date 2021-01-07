4944dd11fa08ca378854a11820fa00a566b51a6b
Julia Ayres <j.bobrineva@gmail.com>
2021-01-07 16:02:01 -0500

    python practice

---
+++ serial.py
@@ -0,0 +1,36 @@
+
+class SerialGenerator:
+    """Machine to create unique incrementing serial numbers.
+
+    >>> serial = SerialGenerator(start=100)
+
+    >>> serial.generate()
+    100
+
+    >>> serial.generate()
+    101
+
+    >>> serial.generate()
+    102
+
+    >>> serial.reset()
+
+    >>> serial.generate()
+    100
+    """
+
+    def __init__(self, start):
+        '''Create a new generator'''
+        self.start = self.next = start
+
+    def __repr__(self):
+        return f"<SerialGenerator start={self.start} next={self.next}>"
+
+    def generate(self):
+        '''Add +1 to previous number'''
+        self.next += 1
+        return self.next - 1
+
+    def reset(self):
+        '''Reset number to starting number'''
+        self.next = self.start
